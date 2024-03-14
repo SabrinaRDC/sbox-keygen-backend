b8d147cb8989ced5a5c3ec582c36058e109a002c
SabrinaRDC <SabrinaRDC.Dev+github@gmail.com>
2024-03-15 00:51:25 +0800

    added function to drop all duplicated concurrent connection from same ip

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ function query(SQLquery, data){
 }
 
 function delayedResEnd(endValue, res, ip) {
-    setTimeout(() => {
+    setTimeout(async () => {
+        await query('UPDATE `sbox-keygen`.ips SET connected = 0 WHERE ip = ?', [ip]);
         console.log(`${endValue} sent to ${ip}`)
         res.end(endValue);
     }, wait)
@@ -46,12 +47,23 @@ const server = http.createServer( async (req, res) => {
     let ipsData;
     let keysData;
     let usedKeysData;
+    //Drop favicon requests
+    if (req.url === '/favicon.ico') {
+        return res.end();
+    };
     console.log(ip, req.method, req.url);
     res.setHeader('Content-Type', 'Text');
     res.setHeader('Access-Control-Allow-Origin', '*');
     ipsData = await query('SELECT * FROM `sbox-keygen`.ips WHERE ip = ?;', [ip])
     keysData = await query('SELECT * FROM `sbox-keygen`.keys');
     usedKeysData = await query('SELECT * FROM `sbox-keygen`.keys WHERE status = ?', ['used']);
+    // Check for existing connection
+    if (ipsData[0] && ipsData[0].connected === 1) {
+        console.log(`Duplicated connection from ${ip} dropped.`);
+        return res.end('Server connection Dropped by DDoS protection.');
+    };
+    // Set ip to connected
+    await query('UPDATE `sbox-keygen`.ips SET connected = 1 WHERE ip = ?', [ip]);
     // Check for existing record
     if (!ipsData[0]) {
         console.log('No record found for', ip)
@@ -60,12 +72,10 @@ const server = http.createServer( async (req, res) => {
         console.log(`Record found for ${ip} found with ${ipsData[0].fetches_left} fetches remaining. Banned: ${ipsData[0].banned}. Name: ${ipsData[0].name}. Tried ${ipsData[0].times_tried} times.`)
     };
     ipsData = await query('SELECT * FROM `sbox-keygen`.ips WHERE ip = ?;', [ip]);
-    // Increment tries counter of ip if not favicon request
-    if (req.url.slice(1) !== 'favicon.ico') {
-        query('UPDATE `sbox-keygen`.ips SET times_tried = ? WHERE ip = ?;', [ipsData[0].times_tried + 1, ip]);
-    };
-    // Set name to input if not blank or favicon request
-    if (req.url.slice(1) !== '' && req.url.slice(1) !== 'favicon.ico') {
+    // Increment tries counter of ip
+    query('UPDATE `sbox-keygen`.ips SET times_tried = ? WHERE ip = ?;', [ipsData[0].times_tried + 1, ip]);
+    // Set name to input if not blank
+    if (req.url.slice(1) !== '') {
         query('UPDATE `sbox-keygen`.ips SET name = ? WHERE ip = ?;', [req.url.slice(1), ip])
     };
     // Check for banned flag on ip
@@ -73,7 +83,7 @@ const server = http.createServer( async (req, res) => {
         return delayedResEnd('Error: This IP has been banned from s&box server due to suspected malicious activities.', res, ip);
     };
     // Respond with random real key if ip does not have banned flag and has >0 real key fetches
-    if (ipsData[0] && ipsData[0].banned === 0 && ipsData[0].fetches_left > 0 && req.url.slice(1) !== 'favicon.ico') {
+    if (ipsData[0] && ipsData[0].banned === 0 && ipsData[0].fetches_left > 0) {
         let realKey = keysData[ Math.floor(Math.random() * keysData.length)];
         query('UPDATE `sbox-keygen`.keys SET times_fetched = ? WHERE id = ?', [realKey.times_fetched + 1, realKey.id]);
         query('UPDATE `sbox-keygen`.ips SET fetches_left = ? WHERE ip = ?;', [ipsData[0].fetches_left - 1, ip]);
