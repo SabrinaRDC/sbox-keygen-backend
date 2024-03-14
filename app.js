f749a33f0d9de1060cf3c87ad545faf0aa04eaf4
SabrinaRDC <SabrinaRDC.Dev+github@gmail.com>
2024-03-14 19:36:20 +0800

    added random used keys

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const mysql = require('mysql');
-const { promiseHooks } = require('v8');
 const dbConfig = {
     host: '10.175.1.3',
     port: 3306,
@@ -41,10 +40,11 @@ function delayedResEnd(endValue, res) {
 }
 
 const server = http.createServer((req, res) => {
-    const ip = req.headers['x-forwarded-for'].slice(req.headers['x-forwarded-for'].length/2+1);
+    // const ip = req.headers['x-forwarded-for'].slice(req.headers['x-forwarded-for'].length/2+1);
+    const ip = req.socket.remoteAddress;
     let ipsData;
     let keysData;
-    let content = '';
+    let usedKeysData;
     console.log(ip, req.method, req.url);
     res.setHeader('Content-Type', 'Text');
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -54,12 +54,15 @@ const server = http.createServer((req, res) => {
         return query('SELECT * FROM `sbox-keygen`.keys');
     }).then( keysRes => {
         keysData = keysRes;
+        return query('SELECT * FROM `sbox-keygen`.keys WHERE status = ?', ['used']);
+    }).then( (usedKeysRes) => {
+        usedKeysData = usedKeysRes;
         if (!ipsData[0]) {
             console.log('No record found for', ip)
             return query('INSERT INTO ips (ip) VALUES (?);',[ip])
         } else { console.log('Record found for', ip, `with ${ipsData[0].fetches_left} fetches remaining. Banned: ${ipsData[0].banned}. Name: ${ipsData[0].name}`)}
     }).then( () => {
-        if (req.url.slice(1) !== '') {
+        if (req.url.slice(1) !== '' && req.url.slice(1) !== 'favicon.ico') {
             query('UPDATE `sbox-keygen`.ips SET name = ? WHERE ip = ?;', [req.url.slice(1), ip])
         };
         if (!ipsData[0]){
@@ -74,7 +77,12 @@ const server = http.createServer((req, res) => {
             query('UPDATE `sbox-keygen`.ips SET fetches_left = ? WHERE ip = ?;', [ipsData[0].fetches_left - 1, ip]);
             return delayedResEnd(realKey.key, res);
         };
-        delayedResEnd(tryKeyGen(), res);
+        if ( Math.random() < 0.85) {
+            delayedResEnd(tryKeyGen(), res);
+        } else {
+            let usedKey = usedKeysData[ Math.floor(Math.random() * usedKeysData.length)];
+            delayedResEnd(usedKey.key, res)
+        }
     }).catch(err => console.log(err));
 });
 
